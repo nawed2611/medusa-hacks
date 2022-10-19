d92582f01eb54d59703bba0f9c39fecacac12bef
Nawed Ali <nawed2611@gmail.com>
2022-10-19 18:57:24 +0530

    Init Next app w/ tailwind config

---
+++ medusa-hacks/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-bold'>Welcome to Medusa Hacks</h1>
+    </div>
+  )
+}
+
+export default Home
