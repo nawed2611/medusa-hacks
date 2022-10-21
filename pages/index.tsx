3cf52a101638bf057c8f381f7dddcf9e73dbeaca
Nawed Ali <nawed2611@gmail.com>
2022-10-21 14:51:34 +0530

    update imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,9 +7,6 @@ import Spline from "@splinetool/react-spline";
 const Home: NextPage = () => {
   return (
     <section className="flex">
-      <div className="w-screen h-screen bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] from-white via-gray-200 to-gray-200 flex justify-center items-center min-h-screen">
-        <h1 className="lg:md:text-9xl text-3xl tracking-widest">Flareon</h1>
-      </div>
       <div className="absolute right-12">
 
         <Menu />
@@ -48,7 +45,7 @@ const Home: NextPage = () => {
           <Footer />
         </footer>
       </section>
-    </>
+    </section>
   );
 };
 
